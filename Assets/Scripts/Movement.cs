9a71e4c06ee3cae7d6adf7f5517f52f1384ebbb1
nicktwright88 <nicktwright88@gmail.com>
2019-03-23 21:20:39 -0700

    Added baseline leveling system, bug fixes
    -Added Player and Tile levels
    -Linked levels together--tile levels are points for player levels
    -Added sliders to track individual tile levels and overall player level
    -Fixed save file backup bug leading to loading the smaller (likely corrupt) file

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -23,11 +23,20 @@ public class Movement : MonoBehaviour {
     public Text tileCountText;
     public Text visitCountText;
     public Text secondCountText;
+    public Text playerLevelText;
+    public Text playerPointCountText;
+    public Text currentTileLevelText;
+    public Text currentTileSecondCountText;
+    public Text currentTileVisitCountText;
+
+    //Unity UI Elements
+    public Slider playerLevelBar;
+    public Slider tileLevelBar;
 
     //lat&long
     LocationInfo li;
 
-    //lat/long measurements
+    //Measurements
     public float latitude = -91f;
     public float longitude = -181f;
     int newSquareLat = -91;
@@ -39,19 +48,24 @@ public class Movement : MonoBehaviour {
     int tileCount = 0;
     int visitCount = 0;
     int secondCount = 0;
+    int playerLevel = 0;
+    int playerPointCount = 0;
 
     //game vars
     bool homeSet = false;
     Vector3 newPosition;
     bool noSavedFile = false;
     bool saveFileSynced = false;
+    String saveFilename;
+    String backupFilename;
+    int[] levelPoints = new int[101];
 
     Dictionary<string, TileData> dict = new Dictionary<string,TileData>();
 
     //used to approximate GPS to game board. Higher = more precision, less stability
     int reducer = 10000; //default 10000
 
-    //used for setting up Service (for running in background)
+    //used for setting up Service (for running in background) (not working right now)
     AndroidJavaClass unityClass;
     AndroidJavaObject unityActivity;
     AndroidJavaClass customClass;
@@ -59,6 +73,10 @@ public class Movement : MonoBehaviour {
     // Use this for initialization
     IEnumerator Start ()
     {
+        //loading data (if any)
+        saveFilename = Application.persistentDataPath + "/map.dat";
+        backupFilename = Application.persistentDataPath + "/mapB.dat";
+
         LoadData();
 
         //Initialize GPS Service
@@ -88,6 +106,7 @@ public class Movement : MonoBehaviour {
             yield break;
         }
 
+        //log current "successful" location status
         Debug.Log(Input.location.status);
 
         //start getting GPS location every second
@@ -98,11 +117,23 @@ public class Movement : MonoBehaviour {
         Debug.Log("Saving the game every 10 seconds");
         InvokeRepeating("SaveData", 0.0f, 10f);
 
-        while (newSquareLong == 0 && newSquareLat == 0)
+        while (newSquareLat == 0 && newSquareLong == 0)
         {
             Debug.Log("Waiting for current GPS Location...");
             yield return new WaitForSeconds(1);
         }
+
+        //Set up array of total point values for each level
+        for (int i = 0; i <= 100; i++)
+        {
+            levelPoints[i] = (int)Math.Round((double)((4 * Math.Pow(i,3)) / 5));
+            if (i > 0)
+            {
+                levelPoints[i] += levelPoints[i - 1];
+            }
+        }
+
+        playerLevelBar.value = CalculatePlayerLevelBar();
     }
 	
 	// Update is called once per frame
@@ -152,8 +183,7 @@ public class Movement : MonoBehaviour {
         }
         else if (Input.location.status != LocationServiceStatus.Running)
         {
-            Debug.Log("Location Services Status not Running. Status: ");
-            Debug.Log(Input.location.status);
+            Debug.Log("Location Services Status not Running. Status: " + Input.location.status);
         }
         else
         {
@@ -184,19 +214,13 @@ public class Movement : MonoBehaviour {
 
             if (newSquareLat != currSquareLat || newSquareLong != currSquareLong)
             {
-                //Debug.Log("PositionUpdated/ing");
                 UpdatePosition(newSquareLong, newSquareLat);
+            } else
+            {
+                string currentPosition = GetCurrentPositionText();
+                IncrementSeconds(currentPosition);
             }
         }
-
-
-        IncrementSeconds();
-
-        Debug.Log("ADDING SECOND");
-        int zeroNewSquareLong = newSquareLong - homeSquareLong;
-        int zeroNewSquareLat = newSquareLat - homeSquareLat;
-        string currentPosition = zeroNewSquareLong + "," + zeroNewSquareLat;
-        dict[currentPosition].AddSecond();
     }
 
     void SetHomeGPS(int setSquareLat = -91, int setSquareLong = -181)
@@ -205,16 +229,15 @@ public class Movement : MonoBehaviour {
         
         homeSquareLat = setSquareLat;
         homeSquareLong = setSquareLong;
-        homeSquareLongText.text = homeSquareLong.ToString();
         homeSquareLatText.text = homeSquareLat.ToString();
+        homeSquareLongText.text = homeSquareLong.ToString();
         homeSet = true;
     }
 
     void UpdatePosition(int newSquareLong, int newSquareLat)
     {
-
-        int zeroNewSquareLong = newSquareLong - homeSquareLong;
         int zeroNewSquareLat = newSquareLat - homeSquareLat;
+        int zeroNewSquareLong = newSquareLong - homeSquareLong;
 
         newPosition = new Vector3(zeroNewSquareLong, zeroNewSquareLat);
         rb2.position = newPosition;
@@ -223,11 +246,15 @@ public class Movement : MonoBehaviour {
         currSquareLong = newSquareLong;
         
         UpdateTile(zeroNewSquareLong, zeroNewSquareLat);
+
+        string currentPosition = zeroNewSquareLong + "," + zeroNewSquareLat;
+
+        DisplayCurrentTileInfo(currentPosition);
     }
 
     void UpdateTile(float rbx, float rby)
     {
-        Debug.Log("Adding Tile");
+        
         string currentPosition = rbx + "," + rby;
         if (!dict.ContainsKey(currentPosition))
         {
@@ -239,46 +266,201 @@ public class Movement : MonoBehaviour {
                             new Vector3(rbx, rby), Quaternion.identity) as GameObject;
             }
             IncrementTiles();
-            Debug.Log(currentPosition);
         }
-        Debug.Log("INCREMENT VISITS BY 1");
-        IncrementVisits();
-        dict[currentPosition].AddVisit();
+        IncrementSeconds(currentPosition);
+        IncrementVisits(currentPosition);
     }
 
     void IncrementTiles()
     {
         tileCount++;
-        tileCountText.text = "Tiles: " + tileCount.ToString();
+        tileCountText.text = "Total Tiles: " + tileCount.ToString();
     }
 
-    void IncrementVisits(int visits = 1)
+    void IncrementVisits(string currentPosition)
+    {
+        //Add a visit to the tile
+        dict[currentPosition].AddVisit();
+        visitCount++;
+        visitCountText.text = "Total Visits: " + visitCount.ToString();
+
+        //checks the total points to determine the correct level
+        int newLevel = CheckNextLevel(dict[currentPosition].secondCount, dict[currentPosition].tileLevel);
+
+        //Add the tile level to the player points
+        AddPointsToPlayer(dict[currentPosition].tileLevel);
+    }
+
+    //seconds act as points for each tile
+    void IncrementSeconds(String currentPosition)
+    {
+        dict[currentPosition].AddSecond();
+        secondCount++;
+        secondCountText.text = "Total Seconds: " + secondCount.ToString();
+
+        if (currentPosition.Equals(GetCurrentPositionText()))
+        {
+            currentTileSecondCountText.text = "Current Tile Seconds: " + dict[currentPosition].secondCount.ToString();
+        }
+
+        //checks the total points to determine the correct level
+        int newLevel = CheckNextLevel(dict[currentPosition].secondCount, dict[currentPosition].tileLevel);
+
+        if (newLevel > dict[currentPosition].tileLevel)
+        {
+            SetTileLevel(currentPosition, newLevel);
+        }
+
+        tileLevelBar.value = CalculateTileLevelBar();
+    }
+
+    void SetTileLevel(String currentPosition, int newLevel)
+    {
+        dict[currentPosition].tileLevel = newLevel;
+
+        if (currentPosition.Equals(GetCurrentPositionText()))
+        {
+            currentTileLevelText.text = "Current Tile Level: " + dict[currentPosition].tileLevel.ToString();
+        }
+
+        tileLevelBar.value = CalculateTileLevelBar();
+    }
+
+    public void AddPointsToPlayer(int tileLevel)
+    {
+        playerPointCount += tileLevel;
+        int newLevel = CheckNextLevel(playerPointCount, playerLevel);
+
+        Debug.Log("INITIAL!");
+
+        Debug.Log(newLevel);
+
+        if (newLevel > playerLevel)
+        {
+            SetPlayerLevel(newLevel);
+        }
+        playerPointCountText.text = "Player Points: " + playerPointCount.ToString();
+        playerLevelBar.value = CalculatePlayerLevelBar();
+    }
+
+    public void SetPlayerLevel(int newLevel)
+    {
+        playerLevel = newLevel;
+        playerLevelText.text = "Player Level: " + newLevel.ToString();
+        playerLevelBar.value = CalculatePlayerLevelBar();
+    }
+
+    public String GetCurrentPositionText()
+    {
+        int zeroNewSquareLat = newSquareLat - homeSquareLat;
+        int zeroNewSquareLong = newSquareLong - homeSquareLong;
+
+        return zeroNewSquareLong + "," + zeroNewSquareLat;
+    }
+
+    public float CalculatePlayerLevelBar()
+    {
+        float pointsSoFarThisLevel = (float)playerPointCount - (float)levelPoints[playerLevel];
+        float fullPointsThisLevel = (float)levelPoints[playerLevel + 1] - (float)levelPoints[playerLevel];
+        return pointsSoFarThisLevel / fullPointsThisLevel;
+    }
+
+    public float CalculateTileLevelBar()
+    {
+        String currentPosition = GetCurrentPositionText();
+
+
+        float pointsSoFarThisLevel = (float)dict[currentPosition].secondCount - (float)levelPoints[dict[currentPosition].tileLevel];
+        float fullPointsThisLevel = (float)levelPoints[dict[currentPosition].tileLevel + 1] - (float)levelPoints[dict[currentPosition].tileLevel];
+        return pointsSoFarThisLevel / fullPointsThisLevel;
+    }
+
+    //for use during every new tile visit
+    public void DisplayCurrentTileInfo(String currentPosition)
+    {
+        currentTileLevelText.text = "Current Tile Level: " + dict[currentPosition].tileLevel.ToString();
+        currentTileSecondCountText.text = "Current Tile Seconds: " + dict[currentPosition].secondCount.ToString();
+        currentTileVisitCountText.text = "Current Tile Visits: " + dict[currentPosition].visitCount.ToString();
+        tileLevelBar.value = CalculateTileLevelBar();
+    }
+
+    //for use during initialization only
+    void AddTotalVisits(int visits)
     {
         visitCount += visits;
-        visitCountText.text = "Visits: " + visitCount.ToString();
+        visitCountText.text = "Total Visits: " + visitCount.ToString();
     }
-
-    void IncrementSeconds(int seconds = 1)
+    
+    //for use during initialization only
+    void AddTotalSeconds(int seconds)
     {
         secondCount += seconds;
-        secondCountText.text = "Seconds: " + secondCount.ToString();
+        secondCountText.text = "Total Seconds: " + secondCount.ToString();
+        tileLevelBar.value = CalculateTileLevelBar();
+    }
+
+    //for use during Initialization only
+    void SetTotalPlayerPoints(int points)
+    {
+        playerPointCount += points;
+        playerPointCountText.text = "Player Points: " + playerPointCount.ToString();
+        playerLevelBar.value = CalculatePlayerLevelBar();
+    }
+
+    public int CheckNextLevel(int points, int level)
+    {
+        
+        int newLevel = -1;
+        if (levelPoints[level] > points)
+        {
+            //TODO?: Return CORRECT level if somehow the level is too high. This should never happen, though.
+            newLevel = -1;
+        } else {
+            for (int i = level + 1; i <= 100; i++)
+            {
+                if (newLevel == -1 && levelPoints[i] > points)
+                {
+                    newLevel = i-1;
+                }
+            }
+            //newLevel = 100;
+        }
+        return newLevel;
     }
 
     void SaveData()
     {
+        String saveFilename = Application.persistentDataPath + "/map.dat";
         if (noSavedFile || saveFileSynced)
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Create(Application.persistentDataPath + "/map.dat");
+            FileStream file = File.Create(saveFilename);
+            FileStream backupFile = File.Create(backupFilename);
 
             MapMap data = new MapMap();
             data.saved = "YES";
             data.dict = dict;
+            data.playerLevel = playerLevel;
+            data.playerPointCount = playerPointCount;
             data.homeSquareLat = homeSquareLat;
             data.homeSquareLong = homeSquareLong;
 
-            bf.Serialize(file, data);
+            if (!noSavedFile && saveFileSynced)
+            {
+                bf.Serialize(backupFile, data);
+                long backupLength = new FileInfo(backupFilename).Length;
+                long saveLength = new FileInfo(saveFilename).Length;
+                if (backupLength > saveLength)
+                {
+                    System.IO.File.Copy(backupFilename, saveFilename, true);
+                }
+            } else if (saveFileSynced)
+            {
+                bf.Serialize(file, data);
+            }
+
             file.Close();
+            backupFile.Close();
             
             saveFileSynced = true;
             noSavedFile = false;
@@ -287,13 +469,12 @@ public class Movement : MonoBehaviour {
 
     void LoadData()
     {
-        String saveFilename = Application.persistentDataPath + "/map.dat";
-
         if (!File.Exists(saveFilename)) {
             Debug.Log("File doesn't exist.");
             noSavedFile = true;
         }
-        else if (new FileInfo(saveFilename).Length == 0)
+        else if (new FileInfo(saveFilename).Length == 0 &&
+                    new FileInfo(backupFilename).Length == 0)
         {
             Debug.Log("File length is zero.");
             noSavedFile = true;
@@ -302,7 +483,15 @@ public class Movement : MonoBehaviour {
         {
             Debug.Log("Loading File");
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(saveFilename, FileMode.Open);
+            FileStream file;
+
+            if (new FileInfo(backupFilename).Length < new FileInfo(saveFilename).Length)
+            {
+                file = File.Open(saveFilename, FileMode.Open);
+            } else {
+                file = File.Open(backupFilename, FileMode.Open);
+            }
+
             MapMap data = (MapMap)bf.Deserialize(file);
             file.Close();
             
@@ -312,10 +501,9 @@ public class Movement : MonoBehaviour {
             {
                 IncrementTiles();
                 string[] xy = k.Key.Split(',');
-                Debug.Log(xy);
-                Debug.Log("INCREMENT VISITS BY "+k.Value.visitCount);
-                IncrementVisits(k.Value.visitCount);
-                IncrementSeconds(k.Value.secondCount);
+
+                AddTotalVisits(k.Value.visitCount);
+                AddTotalSeconds(k.Value.secondCount);
                 groundClone = Instantiate(groundPrefab,
                                  new Vector3(Int32.Parse(xy[0]), 
                                             Int32.Parse(xy[1])), 
@@ -326,6 +514,10 @@ public class Movement : MonoBehaviour {
             saveFileSynced = true;
 
             SetHomeGPS(data.homeSquareLat, data.homeSquareLong);
+
+            SetPlayerLevel(data.playerLevel);
+
+            SetTotalPlayerPoints(data.playerPointCount);
         }
     }
 }
@@ -337,6 +529,8 @@ class MapMap
     public Dictionary<string, TileData> dict;
     public int homeSquareLat = -91;
     public int homeSquareLong = -181;
+    public int playerLevel = 0;
+    public int playerPointCount = 0;
 
     public int getDictVals()
     {
@@ -349,11 +543,13 @@ class TileData
 {
     public int visitCount;
     public int secondCount;
+    public int tileLevel;
 
     public TileData()
     {
         visitCount = 0;
         secondCount = 0;
+        tileLevel = 0;
     }
 
     public void AddSecond()
